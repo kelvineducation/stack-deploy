59cc1ba6160dbb0f8547f6b422512046a5f2aa25
Matt Light <matt@kelvin.education>
2018-12-23 21:30:11 -0800

    Add more tests

--- tests.sh
+++ tests.sh
@@ -4,6 +4,17 @@ docker build -q -t kelvineducation/stack-deploy:test . || exit $?
 
 has_failures=0
 
+deploy() {
+  stack_file="${1}"
+  stack_name="${2}"
+
+  STACK_FILE="${stack_file}" \
+    STACK_NAME="${stack_name}" \
+    docker-compose -f tests/stack-deploy.yml run --rm stack-deploy >/dev/null
+
+  return $?
+}
+
 run_test() {
   local timestamp=$(date +%s)
   local test_name="${1}"
@@ -11,12 +22,22 @@ run_test() {
 
   local expected_exit=$(sed -E '/^Exit code:/s/^.*([0-9]+)$/\1/' "tests/${test_name}.expected.txt")
 
-  echo -n "Running ${test_name}... "
+  echo -n "Running '${test_name}'... "
 
-  STACK_FILE="${test_name}.stack.yml" \
-    STACK_NAME="${stack_name}" \
-    docker-compose -f tests/stack-deploy.yml run --rm stack-deploy >/dev/null
+  local setup_stack="${test_name}.setup.yml"
+  local setup_exit=0
+  if [ -f "${setup_stack}" ]; then
+    deploy "${setup_stack}" "${stack_name}"
+    setup_exit=$?
 
+    if [ $setup_exit -ne 0 ]; then
+      echo "setup failed. Setup stack deploy exited with code ${setup_exit}."
+      has_failures=1
+      return
+    fi
+  fi
+
+  deploy "${test_name}.stack.yml" "${stack_name}"
   local actual_exit="$?"
 
   if [ $actual_exit -ne $expected_exit ]; then
@@ -29,7 +50,11 @@ run_test() {
   docker stack rm "${stack_name}" >/dev/null
 }
 
+run_test "works"
+run_test "update-works"
 run_test "initially-broken"
+run_test "becomes-broken"
+
 
 [ $has_failures -eq 0 ] \
   && echo "All tests passed successfully" \
